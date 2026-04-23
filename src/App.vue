521e1059f659ee1263695d262b842d22894abd78
Deerain <1deerain1@gmail.com>
2026-04-23 19:56:12 +0300

    work

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Nebula404 from "./components/Nebula404.vue"
 </script>
 
 <template>
-  <HelloWorld />
+  <Nebula404 />
 </template>
